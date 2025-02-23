feda192c9c3ea3733ec12950ad94b04ca2e5ec78
Melvin Louwerse <melvin@ibuildings.nl>
2025-02-23 12:14:19 +0100

    collisioons

--- index.js
+++ index.js
@@ -2,14 +2,49 @@ const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext('2d')
 const player = new Image()
 const map = new Image();
+const boundaries = []
 
 const SPEED = 3
+const MAP_SIZE = 100;
+
+const collisionMap = [];
+
+for (let i = 0; i < collisions.length; i += MAP_SIZE) {
+    collisionMap.push(collisions.slice(i, i + MAP_SIZE));
+}
+
+class Boundary {
+    constructor({position: tilePosition}) {
+        this.position = tilePosition
+        this.width = 48
+        this.height = 48
+        this.offsetX = -2379
+        this.offsetY = -1010
+    }
+
+    draw() {
+        ctx.fillStyle = "red"
+        const x = (this.position.x * this.width) + this.offsetX
+        const y = (this.position.y * this.height) + this.offsetY
+        ctx.fillRect(x, y, this.width, this.height)
+    }
+}
+
+
+collisionMap.forEach(
+    (row, i) => {
+        row.forEach((symbol, j) => {
+            if (symbol > 0) {
+                boundaries.push(
+                    new Boundary({position: {x: j, y: i}})
+                )
+            }
+        })
+    }
+)
 
 canvas.width = 1024
 canvas.height = 576
-
-ctx.fillStyle = 'white'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 map.src = "./assets/map.png"
 player.src = "./assets/playerDown.png"
@@ -48,6 +83,14 @@ function drawImage() {
         player.width / 4,
         player.height
     )
+
+    boundaries.forEach(
+        (boundary) => {
+            boundary.draw()
+        }
+    )
+
+
 }
 
 const keys = {
